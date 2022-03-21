b5bb4fb62520adf590d362a9c72af72292875548
Magi Natsuki <98022873+MagiNatsukiYTB@users.noreply.github.com>
2022-03-21 20:26:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,45 +33,40 @@ async def giveaway(ctx, time:int, loại_time=None, *,prize:str):
     try:
         if loại_time == None:
             await ctx.send("You need to enter time type and have time types: **s, m, h, d**")
-
-        if prize == None:
-            await ctx.send("You need to enter the prize")
-
         embed = discord.Embed(title=":gift:Giveaway!",colour=discord.Colour.red())
         embed.add_field(name=f"**{prize}**!!", value=f"React:gift:to join\nTime left: {time}{loại_time}\nHosted by: {ctx.author.mention}")
-
+        
         if loại_time == "s":
             gawtime = time * 1
-
         if loại_time == "m":
             gawtime = time * 60
-
         if loại_time == "h":
             gawtime = time * 3600
-
         if loại_time == "d":
             gawtime = time * 86400
-
+            
         embed.set_footer(text=f"Giveaway ends in {time}{loại_time}")
-
         with open('channel.json', 'r') as f:
             channels = json.load(f)
-
         gaw_msg = await client.get_channel(channels[str(ctx.guild.id)]).send(embed=embed)
         await gaw_msg.add_reaction("🎉")
         await ctx.message.delete()
         await asyncio.sleep(gawtime)
+        
         new_gaw_msg = await ctx.channel.fetch_message(gaw_msg.id)
         users = await new_gaw_msg.reactions[0].users().flatten()
         users.pop(users.index(client.user))
         winner = random.choice(users)
+        
         embed.clear_fields()
         embed.add_field(name="Winner!!", value=f"{winner.mention}\nHosted by: {ctx.author.mention}")
         await gaw_msg.edit(embed=embed)
         await ctx.send(f"Winner!! {winner.mention} won {prize}!")
+        
     except:
         await ctx.send("pjoingiveaway to connect giveaway with channel to create giveaway")
 
+        
 @client.command()
 async def joingiveaway(ctx):
     with open('channel.json', 'r') as f:
@@ -84,6 +79,7 @@ async def joingiveaway(ctx):
         json.dump(channels, f, indent=4)
     await ctx.send("Connected to the channel")
 
+  
 @client.command()
 async def help_giveaway(ctx):
     embed = discord.Embed(title="Help Giveaway",colour=discord.Colour.red())
